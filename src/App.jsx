4d9388ffc3301b50e88bf5d1c63e957f134bb430
thelegendaryarticuno <142525587+thelegendaryarticuno@users.noreply.github.com>
2025-04-08 01:21:13 +0530

    fixed app

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,17 @@ import Home from './pages/home';
 import Register from './pages/register';
 import Layout from './pages/layout';
 import './index.css'
-import { Analytics } from "@vercel/analytics/react"
+
 
 function App() {
   return (
     <Router>
       <Routes>
-      <Analytics />
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
           <Route path="/register" element={<Register />} />
         </Route>
       </Routes>
-
     </Router>
   );
 }
